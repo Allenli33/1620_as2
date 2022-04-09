91c4c052fc22c30ad591a35ff4a54d8012ceb4fe
Hao Li <hli197@my.bcit.ca>
2022-04-08 17:20:59 -0700

    added new elements to app.js (save button and cancel button)

--- src/app.js
+++ src/app.js
@@ -5,3 +5,23 @@ const notes = [
     id: 1 
   }
 ]
+
+//save and cancel button
+const SaveBtn = document.createElement('button')
+SaveBtn.innerHTML = 'Save'
+SaveBtn.id = 'save-button';
+
+
+const CancelBtn = document.createElement('button')
+CancelBtn.innerHTML = 'Cancel'
+CancelBtn.id = 'Cancel-button';
+
+
+// Create note taking area
+const CreateText = document.querySelector('.icons')
+const WriteText = document.querySelector('.write-note-area')
+const noteArea = <textarea rows="30" cols="40"></textarea>
+function TextArea() {
+
+}
+
