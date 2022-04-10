d032adbce6bdc2300d5fbb49155f900c3bd221e7
Hao Li <hli197@my.bcit.ca>
2022-04-09 20:49:57 -0700

    added new function and event to app.js, which when click plus button will create a textarea.

--- src/app.js
+++ src/app.js
@@ -14,14 +14,22 @@ SaveBtn.id = 'save-button';
 
 const CancelBtn = document.createElement('button')
 CancelBtn.innerHTML = 'Cancel'
-CancelBtn.id = 'Cancel-button';
+CancelBtn.id = 'cancel-button';
 
 
 // Create note taking area
 const CreateText = document.querySelector('.icons')
 const WriteText = document.querySelector('.write-note-area')
-const noteArea = <textarea rows="30" cols="40"></textarea>
-function TextArea() {
-
+const NoteArea = `<textarea class ="new-note"
+  rows="30" cols="60">Write something here</textarea>`
+function CTextArea() {
+  WriteText.insertAdjacentHTML('afterbegin', NoteArea)
+  WriteText.appendChild(SaveBtn)
+  WriteText.appendChild(CancelBtn)
+  CreateText.remove()
 }
+
+CreateText.addEventListener('click', CTextArea)
+
+//Cancel textare
 
