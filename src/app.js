bf3b905dae65346250dea42d35bc8858d43c3a8b
Hao Li <hli197@my.bcit.ca>
2022-04-09 23:28:05 -0700

    added new function to app.js, which click cancel button will remove textearea and also remove save, cancel buttons as well.

--- src/app.js
+++ src/app.js
@@ -17,19 +17,29 @@ CancelBtn.innerHTML = 'Cancel'
 CancelBtn.id = 'cancel-button';
 
 
+
 // Create note taking area
 const CreateText = document.querySelector('.icons')
 const WriteText = document.querySelector('.write-note-area')
-const NoteArea = `<textarea class ="new-note"
+
+const NoteArea = `<textarea id ="new-note"
   rows="30" cols="60">Write something here</textarea>`
 function CTextArea() {
   WriteText.insertAdjacentHTML('afterbegin', NoteArea)
   WriteText.appendChild(SaveBtn)
   WriteText.appendChild(CancelBtn)
-  CreateText.remove()
 }
 
 CreateText.addEventListener('click', CTextArea)
 
-//Cancel textare
 
+//Cancel textare
+function CancelText() {
+  SaveBtn.remove()
+  CancelBtn.remove()
+  let node = document.getElementById("new-note");
+  if (node.parentNode) {
+    node.parentNode.removeChild(node)
+  }
+}
+CancelBtn.addEventListener('click', CancelText)
