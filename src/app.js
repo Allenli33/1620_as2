3f5870012543865d8042ad09f8cc5826203e9a7c
Hao Li <hli197@my.bcit.ca>
2022-04-11 20:54:13 -0700

    added new event and function to app.js which when click save button will save text into side bar notes area.

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const notes = [
   }
 ]
 
+
+
 //save and cancel button
 const SaveBtn = document.createElement('button')
 SaveBtn.innerHTML = 'Save'
@@ -19,21 +21,23 @@ CancelBtn.id = 'cancel-button';
 
 
 // Create note taking area
-const CreateText = document.querySelector('.icons')
-const WriteText = document.querySelector('.write-note-area')
 
+const CreateText = document.querySelector('.create-note-area .icons')
+const WriteText = document.querySelector('.write-note-area')
 const NoteArea = `<textarea id ="new-note"
-  rows="30" cols="60">Write something here</textarea>`
+  rows="30" cols="60"></textarea>`
 function CTextArea() {
   WriteText.insertAdjacentHTML('afterbegin', NoteArea)
   WriteText.appendChild(SaveBtn)
   WriteText.appendChild(CancelBtn)
+  CreateText.remove()
 }
 
 CreateText.addEventListener('click', CTextArea)
 
 
 //Cancel textare
+const createNotearea = document.querySelector('.create-note-area')
 function CancelText() {
   SaveBtn.remove()
   CancelBtn.remove()
@@ -41,5 +45,34 @@ function CancelText() {
   if (node.parentNode) {
     node.parentNode.removeChild(node)
   }
+  createNotearea.appendChild(CreateText)
 }
 CancelBtn.addEventListener('click', CancelText)
+
+// Save notes
+
+const Noteslist = document.querySelector('.notes-list')
+function SaveNotes() {
+	let txtarea = document.querySelector('#new-note').value
+	let title,para;
+	if (txtarea.indexOf("\n")!=-1) {
+		title = txtarea.substr(0, txtarea.indexOf("\n"));
+		para = txtarea.substr(txtarea.indexOf("\n")+1);
+	} else {
+		title = txtarea;
+		para = "";
+	}
+	notes.push({ 
+		title: title, 
+		noteBody: para,
+		id: notes.length + 1 
+	})	
+	let li = document.createElement("li");
+	li.appendChild(document.createTextNode(title));
+	li.setAttribute("id", notes.length-1);
+	Noteslist.appendChild(li);
+	li.addEventListener('click', ReadNote)
+	
+	CancelText()
+}
+SaveBtn.addEventListener('click', SaveNotes)
